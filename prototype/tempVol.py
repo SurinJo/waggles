ade1bb004731b8775a755c1906ec1c42badeb3d1
surinJo <surin.jo.dev@gmail.com>
2019-10-17 12:00:06 -0500

    Upload result in refrigrator 1016

--- prototype/tempVol.py
+++ prototype/tempVol.py
@@ -31,7 +31,7 @@ date = datetime.datetime.now()
 
 #-----------File------------
 fname = "Prototype"+ str(date.month) + str(date.day) + str(date.hour) + ".csv"
-time_interval = 2
+time_interval = 600
 
 
 def difTime():
